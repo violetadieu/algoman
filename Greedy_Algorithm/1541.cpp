cc2e6da286532c6f7facd939d9319adf7ceed4fb
김동현 <akxldk2@naver.com>
2018-10-13 13:30:29 +0900

    BAEKJOON ONLINE JUDGE (www.acmicpc.net)
    Greedy Algorithm Q.1541

--- Greedy_Algorithm/1541.cpp
+++ Greedy_Algorithm/1541.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <string>
 using namespace std;
 
 int main(){
-    int GY[51]={0,};
-    vector<int>number;
-    int N;
-    int result=0;
-    cin>>N;
-    number.resize(N);
-    for(int i=0;i<N;i++){
-        cin>>number[i];
-        if(number[i-1]<0)
-            number[i]-=number[i]*2;
+    string s;
+    string tmp_s;
+    int sum=0;
+    bool flag=true;//괄호의 위치(값을 바꾸면 괄호를 열거나(닫거나)한다)
+    cin>>s;
+    for(int i=0;i<=s.length();i++){
+             if(s[i]=='+'||i==s.length()||s[i]=='-') {//55-50+40-100+10-25
+                 int tmp2=stoi(tmp_s);
+                 if(flag)
+                     sum+=tmp2;
+                 else
+                     sum-=tmp2;
+                 if(s[i]=='-')
+                    flag=false;//flip
+
+                 tmp_s = "";
+             }
+            else{
+             tmp_s+=s[i];
+            }
     }
-    GY[0]=number[0];
-    for(int i=0;i<=N;i++){
-        GY[i]=GY[i-1]+number[i];
-    }
-    cout<<GY[N];
+    cout<<sum;
     return 0;
 }
