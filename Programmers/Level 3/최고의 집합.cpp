520936d0c96941ab5f9a49102e1987aff8699597
DONGHYUN KIM <41700359+violetadieu@users.noreply.github.com>
2020-03-16 16:31:18 +0900

    Q. 최고의 집합
    2020/03/01
    프로그래머스 Level 3
    Q. 최고의 집합
    (https://programmers.co.kr/learn/courses/30/lessons/12938)

---
+++ Programmers/Level 3/최고의 집합.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+vector<int> solution(int n, int s) {
+    vector<int> answer;
+
+    int standard=s/n;
+    if(standard==0){
+        answer.push_back(-1);
+        return answer;
+    }
+    for(int i=0;i<n;i++) {
+        answer.push_back(standard);
+        s -= standard;
+    }
+
+    for(int i=answer.size()-1;i>=0;i--){
+        if(s==0)
+            break;
+        answer[i]++;
+        s--;
+    }
+
+    return answer;
+}
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios::sync_with_stdio(false);
+
+    int n=2;
+    int s=1;
+
+    vector<int>ans=solution(n,s);
+
+    return 0;
+}
